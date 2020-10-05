f8ede1a71154506829981927cd6406ec33d3b917
Gorills <gorivanickiy@gmail.com>
2020-10-05 15:41:39 +0700

    popup and jquery settings

--- src/js/main.js
+++ src/js/main.js
@@ -5,3 +5,24 @@ $(".menu-btn").click(function(e) {
   $(".header").toggleClass('header--active');
 
 })
+
+$(window).scroll(function() {
+  var height = $(window).scrollTop();
+  if(height > 500){
+    $('header').addClass('header--fixed');
+    $('.up').show();
+  } else{
+    $('header').removeClass('header--fixed');
+    $('.up').hide();
+  }
+});
+
+
+$('.show_popup').click(function(e) {
+  e.preventDefault();
+  $('.popup').show();
+  $('.overlay_popup').show();
+})
+$('.overlay_popup').click(function() {
+  $('.overlay_popup, .popup').hide();
+})
